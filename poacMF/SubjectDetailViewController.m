44867f076b7182d97810bb8973b0447895ec7ab8
Chris Vanderschuere <cdvconcepts@gmail.com>
2012-06-11 03:07:37 +0100

    Implemented Subject detail grid view

--- poacMF/SubjectDetailViewController.m
+++ poacMF/SubjectDetailViewController.m
@@ -26,6 +26,7 @@
         }];
         //Set value and reload data
         _subjectTests = subjectTests;
+        self.title = [[[_subjectTests objectAtIndex:0] questionSet] typeName];
         [self.gridView reloadData];
     }
 }
@@ -50,11 +51,12 @@
     if ( UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]) )
     {
         // bring 1024 in to 1020 to make a width divisible by five
-        _gridView.leftContentInset = 2.0;
-        _gridView.rightContentInset = 2.0;
+        self.gridView.leftContentInset = 2.0;
+        self.gridView.rightContentInset = 2.0;
     }
     
-    [self.view addSubview: _gridView];
+    [self.view addSubview: self.gridView];
+    [self.gridView reloadData];
 
 }
 
@@ -63,7 +65,7 @@
 
 - (NSUInteger) numberOfItemsInGridView: (AQGridView *) gridView
 {
-    return ( [self.subjectTests count] );
+    return [self.subjectTests count];
 }
 
 - (AQGridViewCell *) gridView: (AQGridView *) gridView cellForItemAtIndex: (NSUInteger) index
@@ -79,7 +81,7 @@
         {
             // must be the SAME SIZE AS THE OTHERS
             // Yes, this is probably a bug. Sigh. Look at -[AQGridView fixCellsFromAnimation] to fix
-            hiddenCell = [[AQGridViewCell alloc] initWithFrame: CGRectMake(0.0, 0.0, 72.0, 72.0)
+            hiddenCell = [[AQGridViewCell alloc] initWithFrame: CGRectMake(0.0, 0.0, 120, 120)
                                                reuseIdentifier: EmptyIdentifier];
         }
         
@@ -87,20 +89,23 @@
         return ( hiddenCell );
     }
     
-    UITableViewCell * cell = (UITableViewCell *)[gridView dequeueReusableCellWithIdentifier: CellIdentifier];
+    AQGridViewCell * cell = (AQGridViewCell *)[gridView dequeueReusableCellWithIdentifier: CellIdentifier];
     if ( cell == nil )
     {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell = [[AQGridViewCell alloc] initWithFrame: CGRectMake(0.0, 0.0, 120, 120)
+                                     reuseIdentifier: CellIdentifier];
+        UIView *colorView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 120, 120)];
+        colorView.backgroundColor = [UIColor blackColor];
+        [cell.contentView addSubview:colorView];
     }
     
-    cell.textLabel.text = [self.subjectTests objectAtIndex:index];
     
     return ( cell );
 }
 
 - (CGSize) portraitGridCellSizeForGridView: (AQGridView *) gridView
 {
-    return ( CGSizeMake(192.0, 192.0) );
+    return ( CGSizeMake(142.0, 142.0) );
 }
 
 
@@ -112,7 +117,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 - (void) willRotateToInterfaceOrientation: (UIInterfaceOrientation) toInterfaceOrientation
                                  duration: (NSTimeInterval) duration
